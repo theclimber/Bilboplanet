a84266e4702e82c5bb1e4d86b1ab7bfd683308b9
Gregoire de Hemptinne <greg@theclimber.be>
2009-10-12 22:49:59 +0200

    Changing architecture

--- trunk/inscription_contenu.php
+++
@@ -1,8 +0,0 @@
-<h2>Description</h2>
-
-<p><br/>Le <a href="/">NOMDEPLANET</a> est un planet visant à regrouper un ensemble de flux RSS de divers sites/blogs.
-<br/><br/></p>
-
-<h2>La charte de fonctionnement</h2>
-
-<br/><br/></p>

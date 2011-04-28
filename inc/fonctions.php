f84a39b0612d6dab4e7303e969f3ae8202e3d124
Gregoire de Hemptinne <greg@theclimber.be>
2011-04-28 09:00:26 +0200

    Add support for JSON if PHP<=5.1

--- inc/fonctions.php
+++ inc/fonctions.php
@@ -24,6 +24,29 @@
 ***** END LICENSE BLOCK *****/
 ?>
 <?php
+#----------------------------------#
+#   Functions for support of JSON  #
+#----------------------------------#
+if ( !function_exists('json_decode') ){
+	function json_decode2($content, $assoc=false){
+		require_once (dirname(__FILE__).'/lib/json/JSON.php');
+		if ( $assoc ){
+			$json = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
+		} else {
+			$json = new Services_JSON;
+		}
+		return $json->decode($content);
+	}
+}
+
+if ( !function_exists('json_encode') ){
+	function json_encode2($content){
+		require_once (dirname(__FILE__).'/lib/json/JSON.php');
+		$json = new Services_JSON;
+
+		return $json->encode($content);
+	}
+}
 
 #----------------------------------#
 #   Fonction Analytics             #
